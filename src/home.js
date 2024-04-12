5787abde15f80b1a97a925dc55b587230852e686
dylanR96 <dylan.ravenson@gmail.com>
2024-04-12 14:08:23 +0200

    Added select elements with option on first page, styling and part of first page remains

--- src/home.js
+++ src/home.js
@@ -4,6 +4,17 @@ const pageContent = document.getElementById("content");
 
 function renderHomePage() {
   pageContent.textContent = "";
+  const bookingSection = document.createElement("section");
+  const bookingWrapper = document.createElement("div");
+  const bookingOptions = document.createElement("div");
+  bookingSection.appendChild(bookingWrapper);
+  const sectionHeader = document.createElement("h5");
+  sectionHeader.textContent = "Table booking";
+  const bookingHeader = document.createElement("h2");
+  bookingHeader.textContent = "BOOK YOUR TABLE NOW";
+  bookingWrapper.appendChild(sectionHeader);
+  bookingWrapper.appendChild(bookingHeader);
+  bookingWrapper.appendChild(bookingOptions);
 
   const selectFields = [
     {
@@ -18,6 +29,7 @@ function renderHomePage() {
       name: "person",
       CssClass: "",
     },
+
     {
       options: [
         {
@@ -34,8 +46,17 @@ function renderHomePage() {
       options: [
         {
           tag: "option",
-          value: 7,
-          text: ["Time", "09:00-11:00", "12:00-14:00", "18:00-20:00"],
+          value: 8,
+          text: [
+            "Day",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+          ],
         },
       ],
       id: "person",
@@ -47,57 +68,67 @@ function renderHomePage() {
         {
           tag: "option",
           value: 3,
-          text: [
-            {
-              option0: "Time",
-              option1: "09:00-11:00",
-              option2: "12:00-14:00",
-              option3: "18:00-20:00",
-            },
-          ],
+          text: ["Time", "09:00-11:00", "12:00-14:00", "18:00-20:00"],
         },
       ],
-      id: "person",
-      name: "person",
+      id: "time",
+      name: "time",
       CssClass: "",
-    },
-    {
-      tag: "textarea",
-      id: "specialMessage",
-      name: "message",
-      rows: "5",
-      cols: "60",
-      placeholder: "Special message",
-      CssClass: "booking__text-area",
     },
   ];
 
-  selectFields.forEach(
-    ({ options, id, name, rows, cols, placeholder, tag = "select" }) => {
-      const selectElement = document.createElement(tag);
-      selectElement.setAttribute("id", id);
-      selectElement.setAttribute("name", name);
-      if (rows) {
-        selectElement.setAttribute("rows", rows);
-        selectElement.setAttribute("cols", cols);
-        selectElement.setAttribute("placeholder", placeholder);
-      }
+  selectFields.forEach(({ options, id, name, tag = "select" }) => {
+    const selectElement = document.createElement(tag);
+    selectElement.setAttribute("id", id);
+    selectElement.setAttribute("name", name);
 
-      options.forEach(({ value, text }, index) => {
+    options.forEach(({ value, text }) => {
+      text.forEach((optionText, index) => {
         const optionElement = document.createElement("option");
-        optionElement.setAttribute("value", value);
-        optionElement.textContent = text;
+        optionElement.setAttribute("value", index === 0 ? "" : optionText);
+        optionElement.textContent = optionText;
         if (index === 0) {
           optionElement.setAttribute("disabled", true);
           optionElement.setAttribute("selected", true);
         }
         selectElement.appendChild(optionElement);
       });
+    });
 
-      pageContent.appendChild(selectElement);
-    }
-  );
+    bookingOptions.appendChild(selectElement);
+  });
+  const specialMessage = document.createElement("textarea");
+  specialMessage.id = "specialMessage";
+  specialMessage.name = "message";
+  specialMessage.rows = 5;
+  specialMessage.cols = 60;
+  specialMessage.placeholder = "Special message";
+  specialMessage.classList.add("booking__text-area");
+  bookingWrapper.appendChild(specialMessage);
+
+  pageContent.appendChild(bookingSection);
 }
+
+{
+  /* const topWrapper = document.createElement("div");
+const leftWrapper = document.createElement("div");
+const leftSideHeader = document.createElement("h1");
+leftSideHeader.textContent = "Have a delicious meal with us";
+const leftSideParagraph = document.createElement("p");
+leftSideParagraph.textContent = " take no arguments, and should return a value of any type. React will
+    call your initializer function when initializing the component, and
+    store its return value as the initial";
+const buttonWrapper = document.createElement("div");
+const menuBtn = document.createElement("button");
+const reservationBtn = document.createElement("button");
+menuBtn.textContent = "Menu";
+reservationBtn.textContent = "Reservation";
+const rightWrapper = document.createElement("div");
+
+const btnWrapper = document.createElement("div");
+const btn1 = document.createElement("button");
+const btn2 = document.createElement("button");
+const btn3 = document.createElement("button");
 
 /* <div class="main__top-wrapper">
 <div class="main__left-side">
@@ -125,87 +156,10 @@ function renderHomePage() {
     ><button class="main__image-btn"></button>
   </div>
 </div>
-</div>
+</div> 
 
-<section class="booking">
+  <section class="booking">
 <div class="booking__wrapper">
-  <h5>Table booking</h5>
-
-  <h2>BOOK YOUR TABLE NOW</h2>
-
-  <div class="booking__options">
-    <select name="person" id="person">
-      <option value="person" disabled selected>Person</option>
-
-      <option value="one">1</option>
-
-      <option value="two">2</option>
-
-      <option value="three">3</option>
-
-      <option value="four">4</option>
-
-      <option value="five">5</option>
-
-      <option value="six">6</option>
-
-      <option value="seven">7</option>
-
-      <option value="eight">8</option>
-
-      <option value="nine">9</option>
-
-      <option value="ten">10</option>
-    </select>
-
-    <select name="month" id="month">
-      <option value="month" disabled selected>Month</option>
-
-      <option value="jan">January</option>
-
-      <option value="feb">February</option>
-
-      <option value="mar">March</option>
-
-      <option value="apr">April</option>
-    </select>
-
-    <select name="day" id="day">
-      <option value="day" disabled selected>Day</option>
-
-      <option value="monday">Monday</option>
-
-      <option value="tuesday">Tuesday</option>
-
-      <option value="wednesday">Wednesday</option>
-
-      <option value="thursday">Thursday</option>
-
-      <option value="friday">Friday</option>
-
-      <option value="saturday">Saturday</option>
-
-      <option value="sunday">Sunday</option>
-    </select>
-
-    <select name="time" id="time">
-      <option value="time" disabled selected>Time</option>
-
-      <option value="morning">09:00-11:00</option>
-
-      <option value="afternoon">12:00-14:00</option>
-
-      <option value="evening">18:00-20:00</option>
-    </select>
-  </div>
-  <textarea
-    name=""
-    id=""
-    cols="60"
-    rows="5"
-    class="booking__text-area"
-    placeholder="Special message"
-  ></textarea>
-  <button>Confirm</button>
-</div>
-</section> */
+<div class="booking__options">
+*/
+}
